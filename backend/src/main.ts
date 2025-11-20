75d2620f68c3263dd89c89ab708f7f17ddc660e7
abdrassulov <032390143@ogr.uludag.edu.tr>
2025-11-21 01:43:14 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Разрешаем запросы с любого источника (CORS)
+  
   app.enableCors({
-    origin: '*', // разрешаем все домены, можно указать конкретный, например 'http://localhost:5173'
+    origin: '*', 
   });
 
   await app.listen(process.env.PORT ?? 3001);
